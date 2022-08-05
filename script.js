c4226bf3f1d05c0c8d048f27e28351237da1fd38
mahmod709 <75578092+mahmoud709@users.noreply.github.com>
2022-08-05 17:35:03 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -5,3 +5,19 @@ bar_icon.onclick=function(){
     menue.classList.toggle("active");
 }
 
+// scroll button
+const scroll_btn=document.getElementById('scroll_button');
+
+window.onscroll=function(){
+    if(scrollY>=400){
+        scroll_btn.style.display='block';
+    }
+    else{
+        scroll_btn.style.display='none';
+    }
+}
+
+
+scroll_btn.onclick=function(){
+scroll(0,0);
+}
